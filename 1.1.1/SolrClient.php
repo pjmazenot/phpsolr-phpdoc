28bdae0bd0ad99836db5979d5aa3e8d49cf696e4
jsmith07 <jsmith@linkup.com>
2018-06-27 20:41:50 -0500

    Updated SolrClient::query() return type to SolrQueryResponse (#1)
    * Updated SolrClient::query() return type to SolrQueryResponse
    
    * Fixed issue with changing return type of SolrClient::request instead of SolrClient::query for some versions

--- 1.1.1/SolrClient.php
+++ 1.1.1/SolrClient.php
@@ -354,8 +354,8 @@ final class SolrClient {
 	 * @param SolrParams $query <p>
 	 * A SolrParams object. It is recommended to use SolrQuery for advanced queries.
 	 * </p>
-	 * @return SolrUpdateResponse <p>
-	 * Returns a SolrUpdateResponse on success and throws an exception on failure.
+	 * @return SolrQueryResponse <p>
+	 * Returns a SolrQueryResponse on success and throws an exception on failure.
 	 * </p>
 	 * @throws SolrClientException <p>
 	 * Throws SolrClientException if the client had failed, or there was a connection issue.
